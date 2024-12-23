455edf8216ce5d6df3503058e06994e737676918
mttmystic <mttmystic444@gmail.com>
2024-12-23 04:17:55 +0000

    design of error reporting tests and error handling structure + integration plans

--- devlog.md
+++ devlog.md
@@ -6,3 +6,5 @@ Design decisions about program flow and enforced input structure and plan for ne
 Switch dev environment to ubuntu vm and sublime text. Organization of new design necessitated deleting first attempt at structuring project in favor of OOP and starting with implementation priorities (i.e. defining test casses for class structure of mixer).
 
 Starting point is skeleton of automated mixer main class -- sure to evolve, but basic entry point. Outlined basic steps for initializing the automixer as a blueprint for dev stages. Using branch am_class_structure and subbranch to tackle error reporting (top priority).
+
+Otlined error reporting tests, error class (handler) structure and success/fail, and dev_mode reporting. Designing before bed is fun even if I save the code for another day :)
---
+++ test/error_testing.py
@@ -0,0 +1,27 @@
+#TODO test reporting of errors and collection of errors (such as invalid auio input) to continue processes and report as aggregate
+
+"""
+Defining basic functionality of error handler which is abstracted from the specific types of errors and ideally from how errors are triggered
+
+- Enums : each class of error (for example, validation error for input audio) is defined with enum
+- Enum members : for each class of error the enum members define a string to be formatted -> simple
+- (Advanced) could enum members somehow store information on callback fn or how to process error in more advanced cases? check if this arises
+- Dev mode errors : meta-report on failure of error reporting and failure of code itself (not user error)
+- for dev mode errors use try/except and report which block caused the error
+
+test points:
+- if error enum does not exist then fail
+- if error type ?(enum member) does not exist then fail
+- in each case above the error handler reports (dev mode) that the error has been reported incorrectly (invalid error code)
+- succeed when error category, code, and outcome (i.e. formatted string) match predefined corrrect response
+
+-- btw dev mode should be in config.ini and enabled by default, but that cnfig is hidden from rando/per-use and a gitignore candidate
+
+- specific error types - integration testing after specific other featues implemented + tests defined
+- start by blueprint of code from am init: validation errors with input dir
+- give invalid input in every way possible and ensure error is reported
+
+
+"""
+
+#FOR NOW, define basic error class with one report_err function and one error category, trigger as test
